a3bdedb62773a1d9ecb781c156a2227c54ffbd04
lytex <julianlpc15@gmail.com>
2021-08-17 22:19:36 +0200

    add logging to *_nulls.sh

--- fix_nulls.sh
+++ fix_nulls.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+echo "[$(date --rfc-3339=seconds)]: Started"
 folders=$(cat .folders)
-nulls=$(cat null.log)
+nulls=$(cat null.log | grep -v "\.syncthing.*\.tmp" | grep -v "sync-conflict")
+IFS=$'\n'
 
 for file in $nulls; do
     file=$(echo "$file" | sed "s#\./##")
     found=false
+    echo "[$(date --rfc-3339=seconds)]: Trying to recover file \"$file\""
     for prefix in $folders; do
         candidate="/media/pi/BlueBox/$prefix/media/pi/Linux Backup/$file"
-        if sudo ls "$candidate" > /dev/null && ! (($(sudo xxd -a -c 1 "$candidate" | wc -l) == 3)); then
-            echo "File found: $candidate"
+        if sudo ls "$candidate" > /dev/null && ! rg -a "\x00" "_" && (($(ls -l "$candidate" | awk '{print $5}') > 3)) ; then
+            echo "[$(date --rfc-3339=seconds)]: File found: $candidate"
             found=true
-            echo Copying from "$candidate" to "/media/pi/Linux Backup/$file"
+            echo "[$(date --rfc-3339=seconds)]: " Copying from "$candidate" to "/media/pi/Linux Backup/$file"
             sudo cp "$candidate" "/media/pi/Linux Backup/$file"
             sudo chown pi:pi "$candidate"
             break
@@ -20,6 +23,8 @@ for file in $nulls; do
         fi
     done
     if ! $found; then
-        echo "Unable to find a non-null file $file"
+        echo "[$(date --rfc-3339=seconds)]: Unable to find a non-null file \"$file\""
     fi
 done
+
+echo "[$(date --rfc-3339=seconds)]: Finished"
--- get_nulls.sh
+++ get_nulls.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 LOGFILE="/home/pi/null.log"
+IFS=$'\n'
 
+echo "[$(date --rfc-3339=seconds)]: Started"
 cd /media/pi/Linux\ Backup/
-command="find . -type f | xargs -I _ bash -c '"'if (($(xxd -a -c 1 "_" | wc -l) == 3)); then echo "_"; fi'"'"
+command="find . -type f | xargs -I _ bash -c '"'if rg -a "\x00" "_"; then echo "_"; fi'"'"
 if [[ "$(cat $LOGFILE)" != "$(eval $command)" ]]; then
     echo "$LOGFILE has changed"
     eval $command > $LOGFILE
 else
     echo "$LOGFILE unchanged"
 fi
+
+echo "[$(date --rfc-3339=seconds)]: Finished"
