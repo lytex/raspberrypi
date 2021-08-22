d3a73f2e25e232ce8886e3f924aafafb8111b954
lytex <julianlpc15@gmail.com>
2021-08-22 18:36:09 +0200

    change rg command

--- get_nulls.sh
+++ get_nulls.sh
@@ -5,7 +5,7 @@ IFS=$'\n'
 
 echo "[$(date --rfc-3339=seconds)]: Started"
 cd /media/pi/Linux\ Backup/
-command="find . -type f | xargs -I _ bash -c '"'if rg -a "\x00" "_"; then echo "_"; fi'"'"
+command="sudo rg -al --no-ignore --hidden --multiline --null-data --no-mmap --max-filesize 512M -j2  "\A\x00+\z" | tr '\0' '\n'"
 if [[ "$(cat $LOGFILE)" != "$(eval $command)" ]]; then
     echo "$LOGFILE has changed"
     eval $command > $LOGFILE
