2a89149e9f8180ff7e961ffb3a68bb8fcba7cf1c
lytex <julianlpc15@gmail.com>
2021-08-27 12:43:53 +0100

    add apt commands, use $device

--- unlock_mount.sh
+++ unlock_mount.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# apt install udisks2 cryptsetup
 
 devicelist=`lsblk -lf | grep LUKS | awk '{print $1}'`
 echo -e "The following LOCKED luks devices have been founded:\n$devicelist"
 for device in $devicelist; do
 	device="/dev/$device"
-	vendor=`udevadm info -a -p $(udevadm info -q path -n /dev/sdc1) | grep "ATTRS{vendor}"`
-	model=`udevadm info -a -p $(udevadm info -q path -n /dev/sdc1) | grep "ATTRS{model}"`
+	vendor=`udevadm info -a -p $(udevadm info -q path -n $device) | grep "ATTRS{vendor}"`
+	model=`udevadm info -a -p $(udevadm info -q path -n $device) | grep "ATTRS{model}"`
 	echo -e "Unlocking device:\n$vendor\n$model"
 	unlock=`udisksctl unlock -b $device | sed -e 's/Unlocked .* as //' -e 's/\.//'`
 	path=`udisksctl mount -b $unlock` | sed "s|Mounted $unlock at ||"
